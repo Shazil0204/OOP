270a3d0e04048c826f851d5eea8aefad7f13994d
Shazil0204 <141632242+Shazil0204@users.noreply.github.com>
2023-09-11 11:38:37 +0200

    Task:1
    Completed

---
+++ Tasks-result/The superheroes/The superheroes/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_superheroes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Superhero hulk = new Superhero();
+            Superhero batman = new Superhero();
+            Superhero superman = new Superhero();
+            Superhero spiderman = new Superhero();
+            Superhero wonderwoman = new Superhero();
+            Superhero greenlantern = new Superhero();
+            Superhero doctorstrange = new Superhero();
+            Superhero captainamerica = new Superhero(); 
+        }
+    }
+}
---
+++ Tasks-result/The superheroes/The superheroes/Superhero.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_superheroes
+{
+    class Superhero
+    {
+        // All Variables
+        private string fear;
+        private string realName;
+        private string superpower;
+        private string superheroName;
+        private string costumeDescription;
+        private string secretHideoutLocation;
+        private bool wearmask;
+        private bool wearcostume;
+        private bool secretidentity;
+
+        // Properties
+
+        public string Fear
+        {
+            get { return fear; }
+            private set { fear = value; }
+        }
+
+        public string RealName
+        {
+            get { return realName; }
+            private set { realName = value; }
+        }
+
+        public string Superpower
+        {
+            get { return superpower; }
+            private set { superpower = value; }
+        }
+
+        public string SuperheroName
+        {
+            get { return superheroName; }
+            private set { superheroName = value; }
+        }
+
+        public string CostumeDescription
+        {
+            get { return costumeDescription; }
+            private set { costumeDescription = value; }
+        }
+
+        public string SecretHideoutLocation
+        {
+            get { return secretHideoutLocation; }
+            private set { secretHideoutLocation = value; }
+        }
+
+        public bool WearMask
+        {
+            get { return wearmask; }
+            private set { wearmask = value; }
+        }
+
+        public bool WearCostume
+        {
+            get { return wearcostume; }
+            private set { wearcostume = value;}
+        }
+
+        public bool SecretIdentity
+        {
+            get { return secretidentity; }
+            private set { secretidentity = value; }
+        }
+    }
+}
