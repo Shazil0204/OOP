ce004cbf4c1c6915a7bea844653639e813b50219
Shiz <141632242+Shazil0204@users.noreply.github.com>
2023-09-11 20:12:00 +0200

    task 2 completed

---
+++ Tasks-result/2. Elon/2. Elon/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Elon
+{
+    #region ControllerAndView
+    internal class Program
+    {
+        #region MainMethod
+        static void Main(string[] args)
+        {
+            // Creating car instances
+            RCCar redCar = new RCCar();
+            redCar.Color = "Red";
+
+            RCCar yellowCar = new RCCar();
+            yellowCar.Color = "Yellow";
+
+            // you can add a while loop here and add this in it so the car will continue driving (Like this)
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.Red; 
+                DriveCar(redCar);
+            }
+
+            // Driving and charging the cars
+            DriveCar(redCar);
+            Console.ForegroundColor = ConsoleColor.Red; 
+            Console.ForegroundColor = ConsoleColor.Yellow; 
+            DriveCar(yellowCar);
+
+            Console.ForegroundColor = ConsoleColor.Red; 
+            ChargeCar(redCar);
+            DriveCar(redCar);
+
+            Console.ForegroundColor = ConsoleColor.Yellow; 
+            ChargeCar(yellowCar);
+            DriveCar(yellowCar);
+
+            Console.ResetColor();
+        }
+        #endregion
+
+        #region HelperMethods
+        static void DriveCar(RCCar car)
+        {
+            Console.WriteLine("Driving the car...");
+            car.Drive();
+        }
+
+        static void ChargeCar(RCCar car)
+        {
+            Console.WriteLine("Charging the car...");
+            car.Charge();
+        }
+        #endregion
+    }
+    #endregion
+}
---
+++ Tasks-result/2. Elon/2. Elon/RCCar.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Elon
+{
+    #region Model
+    internal class RCCar
+    {
+        #region Fields
+        private int _batteryCapacity = 100; // Initialized to 100%
+        private int _distanceDriven = 0;    // Initialized to 0 meters
+        private string _color;
+        #endregion
+
+        #region Properties
+        public int BatteryCapacity
+        {
+            get { return _batteryCapacity; }
+            private set { _batteryCapacity = Math.Max(0, value); } // Ensure battery doesn't go below 0%
+        }
+
+        public int DistanceDriven
+        {
+            get { return _distanceDriven; }
+            private set { _distanceDriven = value; }
+        }
+
+        public string Color
+        {
+            get { return _color; }
+            set { _color = value; }
+        }
+        #endregion
+
+        #region Methods
+        public void Drive()
+        {
+            if (_batteryCapacity > 0)
+            {
+                _distanceDriven += 20;
+                _batteryCapacity -= 1;
+                UpdateDisplay();
+            }
+            else
+            {
+                Console.WriteLine("Battery is empty. Car cannot drive!");
+            }
+        }
+
+        public void Charge()
+        {
+            _batteryCapacity = 100;
+            _distanceDriven = 0;
+            UpdateDisplay();
+        }
+
+        private void UpdateDisplay()
+        {
+            Console.WriteLine($"Car Color: {Color}");
+            Console.WriteLine($"Distance Driven: {_distanceDriven} meters");
+            Console.WriteLine($"Battery Life: {_batteryCapacity}%");
+            Console.WriteLine("=================================");
+        }
+        #endregion
+    }
+    #endregion
+}
