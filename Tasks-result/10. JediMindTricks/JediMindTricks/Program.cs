5f67c20d73615aaffe2c5d640ac8a0031fd9db64
Shiz <141632242+Shazil0204@users.noreply.github.com>
2023-09-15 21:51:44 +0200

    task 10 completed

---
+++ Tasks-result/10. JediMindTricks/JediMindTricks/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JediMindTricks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> characters = new Dictionary<string, int>() 
+            { 
+                { "Luke", 1 }, 
+                { "Han", 2 }, 
+                { "Chewbacca", 3 } 
+            };
+
+            characters.Remove("Luke");
+            characters.Add("Anakin", 1);
+            foreach (var items in characters)
+            {
+                Console.WriteLine(items);
+            }
+            Console.ReadLine();
+        }
+    }
+}
