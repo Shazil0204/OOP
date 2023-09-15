5c526d8479997571eb5e6d1dbd07804368ea3d6a
Shiz <141632242+Shazil0204@users.noreply.github.com>
2023-09-15 21:49:20 +0200

    task 9 Completed

---
+++ Tasks-result/9. Playlisten/Playlisten/Model/Track.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playlisten.Model
+{
+    internal class Track
+    {
+        private string _songName;
+        private string _artist;
+        private string _songWriter;
+        private string _addDate;
+        private string _songLength;
+
+        private string songName { get { return _songName; } set { _songName = value;  } }
+        private string artist { get { return _artist; } set { _artist = value; } }
+        private string songWriter { get { return _songWriter; } set { _songWriter = value; } }
+        private string addDate { get { return _addDate; } set { _addDate = value; } }
+        private string songLength { get {  return _songLength; } set { _songLength = value; } }
+
+        #region constructor
+        public Track(string SongName, string Artist, string SongWriter, string AddDate, string SongLength)
+        {
+            SongName = songName;
+
+            Artist = artist;
+
+            SongWriter = songWriter;
+
+            AddDate = addDate;
+
+            SongLength  = songLength;
+        }
+
+        #endregion
+
+    }
+}
